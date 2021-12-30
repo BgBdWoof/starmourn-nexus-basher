d60022c41f7b97a42840019163b373ffa2e6bc61
BgBdWoof <96842529+BgBdWoof@users.noreply.github.com>
2021-12-30 16:21:32 -0400

    Update mobs.js

--- mobs.js
+++ mobs.js
@@ -37,7 +37,7 @@ nb.mobs = [
 	// 20-40
 	//====================
 	//Praviskar
-	"a merciless Bushraki occupier","an avaricious Bushraki slaver","a lightly armed Bushraki guardswoman","a lightly armed Bushraki guardsman",
+	"an intimidating Bushraki officer","a merciless Bushraki occupier","an avaricious Bushraki slaver","a lightly armed Bushraki guardswoman","a lightly armed Bushraki guardsman",
 	"a heavily armed female Bushraki guard","a heavily armed male Bushraki guard",
 	//Oranc
 	"a stealthy poison-tooth","a green-scaled varrana","a great strider","a wind snake","a Krona poacher","a blue-crested ratika",
@@ -49,11 +49,11 @@ nb.mobs = [
     //Locorin
     "a blacktooth coroxodon","an enormous blacktooth coroxodon","a ruby-plumed eskama","a mossy shellback",
     //Kashitir
-    "a nightstalker","an indigo malaca","a juvenile nightstalker","a Grethen silkmoth","a collared malaca",
+    "a nightstalker","a juvenile nightstalker","an indigo malaca","a Grethen silkmoth","a collared malaca",
     //Golpur
     "an enormous drakkafly","an immense Rapasu worm","a flat balkrab","a giant black tarak beetle",
     //Saksar
-    "an immense reptillian predator","a male Selassian cultist","a female Selassion cultist","a sharp-toothed raptor",
+    "a male Selassian cultist","a female Selassion cultist","a sharp-toothed raptor",
     "a pack of small sharp-toothed reptiles","a scale-feathered scorpion bird","a blue-furred primate","a brutish loroi mercenary",
     //Siva
      "a needle-mouthed eel","an immense alpha skulf","a large hairy skulf","a lean-ribbed skulf","a spot-scaled tridactyl","a venomous tridactyl",
@@ -63,27 +63,36 @@ nb.mobs = [
 	//====================
 	//Pylos
     "a tentacle-finned hookmaw", "a slow-moving hunk of living coral", "an algae-covered calciburr",
-    "a depthless puddle of black liquid", "a poison-spitting treefrog","a delicately bulbous blinkfish",
+    "a depthless puddle of black liquid", "a poison-spitting treefog","a delicately bulbous blinkfish",
     //Delta deck
     "a Fatar salvager","an armored salvage bot","a Bushraki mercenary","a Fatar guardian","an Elgan salvager","a Decheeran salvager","a Nusriza salvager",
     //Greenwilds
-    "a luminous eel","a king tangutan","a hulking arboreal lizard","a great white eagle","a giant guerilla beetle","a female tangutan",
+    "a luminous eel","a king tangutan","a hulking arboreal lizard","a great white eagle","a giant guerilla beetle","a female tangutan","a crested great white eagle","an enormous guerilla beetle"
     //Tosmar
     "a vicious haerbist","a tundra snake",
 
 
 
 	//====================
-	// 50-75
+	// 50-65
 	//====================
 	//Anemoi
 	"a rabid cania","a ravenous tigrid","a carnivorous black eagle","a sly cania","a caliginous eagle","a brown-scaled tigrid",
 	//Arrizuri
-	"a ferocious atzaparaki","a sluggish atzaparaki","a lumbering lasama",
-	//Thait
-	"a ferociously toothed moss lizard","a needle-clawed moss lizard","a horned lapine","a three-eyed rosewing",
+	"a ferocious atzaparaki","a sluggish atzaparaki","a lumbering lasama","a sickly atzaparaki",
 	//New Dikamazi
-	"a rampaging construction mech","a dilapidated construction mech","the thrashing root of a cyborg leech orchid","a cyborg leech orchid",
+	"a rampaging construction mech","a dilapidated construction mech","the thrashing root of a cyborg leech orchid","a cyborg leech orchid","a Chrome Skull mercenary",	
+
+
+
+	//====================
+	// 65+
+	//====================
+
+	//Thait
+	"a ferociously toothed moss lizard","a needle-clawed moss lizard","a horned lapine",
+	//deisk
+	"a terrifying mekmavaur",
 	//Jelle
 	"a thrashing heik-il vine","a monstrously huge heik-il vine","a dour Bushraki looter","a menacing Bushraki looter",
 	"a heavily armed male Bushraki guard","a heavily armed female Bushraki guard","an off-duty Bushraki looter","a tough Bushraki drug runner",
@@ -92,11 +101,10 @@ nb.mobs = [
 	//northern usum
 	'a blacktooth coroxodon',"a giant tosmarian condor","an immense rapasu worm","an immense reptilian predator","an overfed skulf",
 	"a roosting tosmarian condor","a lurking blacktooth coroxodon","a foraging zhubeast","an alert giant springer","a large hairy skulf",
-	"a two-headed zhubeast","a juvenile rapasu worm","a coiling skilth snake","a giant springer",	
+	"a two-headed zhubeast","a juvenile rapasu worm","a coiling skilth snake","a giant springer",
 	//prugita
 	"a savage selakki","a somnolent selakki","a stationary sentry bot","a well-armed security bot","a Nabian guard",
-	//deisk
-	"a terrifying mekmavaur",
+	
 	//td
 	"a tentacle-limbed shapeshifter", "an eyeless maw",
 
