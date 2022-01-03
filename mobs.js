538a1bae54a0745cebc9ce43403b46e22fe9a7a6
BgBdWoof <96842529+BgBdWoof@users.noreply.github.com>
2022-01-03 09:49:58 -0400

    Update mobs.js
    - Added wilderness mobs
    - Fixed issues with some agros being out-of-order

--- mobs.js
+++ mobs.js
@@ -18,97 +18,100 @@ nb.mobs = [
 	// 10-20
 	//====================
 	//Oldtown/Lower reaches/Undercity
-	"a glitch-riddled security robot", "an aggressively malfunctioning drone", "a skull-featured female Bushraki gang member",
-	"a skull-featured male Bushraki gang member", "a vibro-blade wielding Bushraki", "a savage dark-cloaked Bushraki",
+	"a glitch-riddled security robot","an aggressively malfunctioning drone","a skull-featured female Bushraki gang member",
+	"a skull-featured male Bushraki gang member","a vibro-blade wielding Bushraki","a savage dark-cloaked Bushraki",
 	//Coriolis
-	"a malfunctioning windmill drone","a venomous copper flitwing",
+	"an electric-yellow niamp","a spine-winged hirune","a malfunctioning windmill drone","a venomous copper flitwing",
 	// Zephyr
 	"a buzzing lake darter",
 	//Scrapston
 	"a grime-slick eel","a needle-mouthed eel","a poisonous eel",
 	//Whittler's hollow
-	"a fat terraworm","a meaty terraworm","an iridescent opal beetle", "a horrifically mutated rodent",
+	"a fat terraworm","a meaty terraworm","an iridescent opal beetle","a horrifically mutated rodent",
 	//Tabby
-	"a Ry'nari mutant wearing looted soldier fatigues","a grotesquely mutated bird", "a bile-skinned Amaian mutant", "a green-glowing Decheeran mutant", "a scab-scaled Ry'nari mutant",
-    "a grotesque Nusriza mutant", "a mutant encased in ancient power armor", "a warty Ry'nari mutant", "a tatter-finned Amaian mutant",
-    "a twisted Decheeran mutant","a spine-skinned Nusriza mutant",
+	"a Ry'nari mutant wearing looted soldier fatigues","a grotesquely mutated bird","a bile-skinned Amaian mutant","a green-glowing Decheeran mutant","a scab-scaled Ry'nari mutant",
+    	"a grotesque Nusriza mutant","a mutant encased in ancient power armor","a warty Ry'nari mutant","a tatter-finned Amaian mutant",
+    	"a twisted Decheeran mutant","a spine-skinned Nusriza mutant",
 
 	//====================
 	// 20-40
 	//====================
+	//Ishbi Wilderness
+	"a fleshy-headed indigo morkwing","a many-legged bosoth snake",
 	//Praviskar
-	"an intimidating Bushraki officer","a merciless Bushraki occupier","an avaricious Bushraki slaver","a lightly armed Bushraki guardswoman","a lightly armed Bushraki guardsman",
+	"a speckle-scaled sophilian","a green-spotted zemani","an intimidating Bushraki officer","a merciless Bushraki occupier","an avaricious Bushraki slaver","a lightly armed Bushraki guardswoman","a lightly armed Bushraki guardsman",
 	"a heavily armed female Bushraki guard","a heavily armed male Bushraki guard",
 	//Oranc
-	"a stealthy poison-tooth","a green-scaled varrana","a great strider","a wind snake","a Krona poacher","a blue-crested ratika",
+	"a tough-skinned nesihorn","a bioluminescent irktin","a stealthy poison-tooth","a green-scaled varrana","a great strider","a wind snake","a Krona poacher","a blue-crested ratika",
 	//Lowtown
 	"a strung-out female Lowtowner","a strung-out male Lowtowner","an oversized heik-il vine","a shifty looking Nath-el","a juvenile heik-il vine","a thrashing heik-il vine","a sickly heik-il vine",
    	//Goribar
-   	"a small loam leech", "a saffron algerion", "a coral algerion", "a moss-hued algerion", "a large loam leech",
-    	"an azure algerion", "an oversized loam leech", "a loam leech",
+   	"a small loam leech","a saffron algerion","a coral algerion","a moss-hued algerion","a large loam leech",
+    	"an azure algerion","an oversized loam leech","a loam leech",
    	//Locorin
-    	"a blacktooth coroxodon","an enormous blacktooth coroxodon","a ruby-plumed eskama","a mossy shellback",
+    	"an eight-eyed hagda","a nightstalker","a feathered draken lizard","a blacktooth coroxodon","an enormous blacktooth coroxodon","a ruby-plumed eskama","a mossy shellback",
     	//Kashitir
     	"a nightstalker","a juvenile nightstalker","an indigo malaca","a Grethen silkmoth","a collared malaca",
     	//Golpur
-    	"an enormous drakkafly","an immense Rapasu worm","a flat balkrab","a giant black tarak beetle",
+    	"a broad-horned stripeback","an enormous drakkafly","an immense Rapasu worm","a flat balkrab","a giant black tarak beetle",
     	//Saksar
-    	"a male Selassian cultist","a female Selassion cultist","a sharp-toothed raptor",
+    	"a spot-pelted kumta","a ruby-pincered skitter","a male Selassian cultist","a female Selassion cultist","a sharp-toothed raptor",
     	"a pack of small sharp-toothed reptiles","a scale-feathered scorpion bird","a blue-furred primate","a brutish loroi mercenary",
     	//Siva
-     	"a needle-mouthed eel","an immense alpha skulf","a large hairy skulf","a lean-ribbed skulf","a spot-scaled tridactyl","a venomous tridactyl",
+     	"a long-necked tuu","a needle-mouthed eel","an immense alpha skulf","a large hairy skulf","a lean-ribbed skulf","a spot-scaled tridactyl","a venomous tridactyl",
 
 	//====================
 	// 40-50
 	//====================
 	//Pylos
-    	"a tentacle-finned hookmaw", "a slow-moving hunk of living coral", "an algae-covered calciburr",
-    	"a depthless puddle of black liquid", "a poison-spitting treefog","a delicately bulbous blinkfish",
+    	"a spine-carapaced hari","a bone-crested sohemuu","a dark-furred and floating fleel","a tentacle-finned hookmaw","a slow-moving hunk of living coral","an algae-covered calciburr",
+    	"a depthless puddle of black liquid","a poison-spitting treefog","a delicately bulbous blinkfish",
     	//Delta deck
     	"a Fatar salvager","an armored salvage bot","a Bushraki mercenary","a Fatar guardian","an Elgan salvager","a Decheeran salvager","a Nusriza salvager",
     	//Greenwilds
-    	"a luminous eel","a king tangutan","a hulking arboreal lizard","a great white eagle","a giant guerilla beetle","a female tangutan","a crested great white eagle",
+    	"a twiggy naxel","a carmine-shelled xuy beetle","a bilious-feathered malentu","a luminous eel","a king tangutan","a hulking arboreal lizard","a great white eagle","a giant guerilla beetle","a female tangutan","a crested great white eagle",
 	"an enormous guerilla beetle",
     	//Tosmar
-    	"a vicious haerbist","a tundra snake",
+    	"a spike-headed kusowo","a fleshy-crested denghoof","a vicious haerbist","a tundra snake",
 
 	//====================
 	// 50-65
 	//====================
 	//Anemoi
-	"a rabid cania","a ravenous tigrid","a carnivorous black eagle","a sly cania","a caliginous eagle","a brown-scaled tigrid",
+	"a diamond-backed scorptail","a spiral-horned kegri","a rabid cania","a ravenous tigrid","a carnivorous black eagle","a sly cania","a caliginous eagle","a brown-scaled tigrid",
 	//Arrizuri
-	"a ferocious atzaparaki","a sluggish atzaparaki","a lumbering lasama","a sickly atzaparaki",
+	"a stalk-eyed mayaki","a fleshy-spined echidna","a ferocious atzaparaki","a sluggish atzaparaki","a lumbering lasama","a sickly atzaparaki",
 	//New Dikamazi
-	"a rampaging construction mech","a dilapidated construction mech","the thrashing root of a cyborg leech orchid","a cyborg leech orchid","a Chrome Skull mercenary",	
+	"a mutated strangler fern","a rampaging construction mech","a dilapidated construction mech","the thrashing root of a cyborg leech orchid","a cyborg leech orchid","a Chrome Skull mercenary",	
 
 
 	//====================
 	// 65+
 	//====================
 	//Thait
-	"a ferociously toothed moss lizard","a needle-clawed moss lizard","a horned lapine",
+	"a purple-spotted fadeti","a star-nosed burrower","a ferociously toothed moss lizard","a needle-clawed moss lizard","a horned lapine",
 	//deisk
-	"a terrifying mekmavaur",
+	"a corkscrewing giant gilworm","a goo-bodied blithu","a terrifying mekmavaur",
 	//Jelle
-	"a thrashing heik-il vine","a monstrously huge heik-il vine","a dour Bushraki looter","a menacing Bushraki looter",
+	"a virile heik-il vine","a tangle-bodied tentacla","a thrashing heik-il vine","a monstrously huge heik-il vine","a dour Bushraki looter","a menacing Bushraki looter",
 	"a heavily armed male Bushraki guard","a heavily armed female Bushraki guard","an off-duty Bushraki looter","a tough Bushraki drug runner",
 	//Ixsei
-	"a crystal-clawed rock giant","a salt-crusted quartz creeper",
+	"a crystal-headed tolma","a silver-scaled spinehusk","a crystal-clawed rock giant","a salt-crusted quartz creeper",
 	//northern usum
-	'a blacktooth coroxodon',"a giant tosmarian condor","an immense rapasu worm","an immense reptilian predator","an overfed skulf",
+	"a spike-shouldered tau","a skeleton-headed gemra","a leathery-winged okrot","a green-fronded prulan","a blacktooth coroxodon","a giant tosmarian condor","an immense rapasu worm","an immense reptilian predator","an overfed skulf",
 	"a roosting tosmarian condor","a lurking blacktooth coroxodon","a foraging zhubeast","an alert giant springer","a large hairy skulf",
 	"a two-headed zhubeast","a juvenile rapasu worm","a coiling skilth snake","a giant springer",
 	//prugita
-	"a savage selakki","a somnolent selakki","a stationary sentry bot","a well-armed security bot","a Nabian guard",
-	
+	"a scoop-tusked gliven","a stinger-footed hiver","a four-eared remes rat","a savage selakki","a somnolent selakki","a stationary sentry bot","a well-armed security bot","a Nabian guard",
+	//Suiko
+	"a twin-headed henizu","a long-legged trisurde","a metallic-furred fuber",
 	//td
-	"a tentacle-limbed shapeshifter", "an eyeless maw",
+	"a tentacle-limbed shapeshifter","an eyeless maw",
 
 ];
 
 //names of mechanical targets go here. 
 nb.mechanicals = ["a cyborg leech orchid","a rampaging construction mech","a dilapidated construction mech","a stationary sentry bot","a well-armed security bot",
-"a glitch-riddled security robot", "an aggressively malfunctioning drone","a malfunctioning windmill drone","an armored salvage bot"];
+"a glitch-riddled security robot","an aggressively malfunctioning drone","a malfunctioning windmill drone","an armored salvage bot"];
 
 nb.ignores = nb.ignores || [];
